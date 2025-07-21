34ff6797aed7f0f8ff5958ef35fcc46266d36b60
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-07-22 01:14:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TELEGRAM_BOT_TOKEN = "7969712860:AAH1-8cEfDkprnqoBbn_XFwflyV5-9_FJrk"
 GEMINI_API_KEY = "AIzaSyA8XZNchXZRNM3il3Lqb0Y4BiU-COqmu2w"
 OWNER_ID = 7448520005
+MONGO_DB_URL = "mongodb+srv://Music:power@music.fao49li.mongodb.net/?retryWrites=true&w=majority&appName=Music"
